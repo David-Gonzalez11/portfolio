ea516bee93b8f858afbdb999862302afa4351a4a
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2024-01-21 22:06:04 -0800

    Working on portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import logo from "./logo.svg"
 import "./App.css"
-
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Col from "react-bootstrap/Col"
+import Image from "react-bootstrap/Image"
+import { Link } from "react-router-dom"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { faLinkedin} from "@fortawesome/free-brands-svg-icons"
 function App() {
   return (
-    <div className="App">
-      <nav className="nav-links"><span>LinkedIn</span> <span>GitHub</span></nav>
-      <header className="App-header">
-        <img  src="https://media.istockphoto.com/id/1330180806/vector/blue-sign-design-for-photo-placeholder-on-app-design-user-interface-design-avatar-placeholder.jpg?s=612x612&w=0&k=20&c=jMgBjBh-shvWi0ojpkHSMFnTS9vLXFBKws_VLFhK2eo=" alt="profile-img"/>
-        <p>Hey!, This is David Gonzalez</p>
-        <div>
-          <p>Front End Web Developer</p>
-        </div>
-      </header>
+    <div className="">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Nav.Link>
+          <a
+            href="https://www.linkedin.com/in/david-gonzalez2/"
+            className="links resume"
+          >
+            <span>Resume</span>
+          </a>
+        </Nav.Link>
+      </Navbar>
+
+      <br />
+      <div className="App-header">
+        {/* <div className="connect-icons d-flex justify-content-center"> */}
+        <FontAwesomeIcon icon={faLinkedin} className="text-white icons" />
+        <FontAwesomeIcon icon={faSquareGithub} className="text-white icons" />
+      </div>
+      {/* </div> */}
     </div>
   )
 }
